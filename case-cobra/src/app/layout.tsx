3a3fc2d88881bdfa09891600c2c18e1ee2af3d6d
Sofroniebruh <esnu@MacBook-Air-Sofronie.local>
2024-12-27 08:09:08 +0400

    main page done

--- case-cobra/src/app/layout.tsx
+++ case-cobra/src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar-component";
+import { KindeProvider } from "@kinde-oss/kinde-auth-nextjs";
+import { AuthProvider } from "./AuthProvider";
+import Footer from "@/components/footer-component";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +27,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Navbar></Navbar>
+          {children}
+          <Footer></Footer>
+        </body>
+      </html>
+    </AuthProvider>
   );
 }
--- case-cobra/src/components/max-width-component.tsx
+++ case-cobra/src/components/max-width-component.tsx
@@ -5,7 +5,7 @@ export default function MaxWidth({
     className,
     children,
 }: {
-    className: string,
+    className?: string,
     children: ReactNode,
 }) {
     return (
